473f8a092d9d249d73227091a62405e53ac85896
aferey <kwodhan@gmail.com>
2017-10-28 18:51:56 +0200

    clean some files

--- src/main/java/fr/istic/taa/weekEndProject/jpa/Application.java
+++ src/main/java/fr/istic/taa/weekEndProject/jpa/Application.java
@@ -3,6 +3,7 @@ package fr.istic.taa.weekEndProject.jpa;
 import java.io.File;
 
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.LoggerContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,31 +20,22 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @ComponentScan(basePackages = { "fr.istic.taa.weekEndProject.controller", "fr.istic.taa.weekEndProject.service" })
 @EntityScan("fr.istic.taa.weekEndProject.model")
 @EnableJpaRepositories("fr.istic.taa.weekEndProject.repository")
-@ImportResource({ "applicationContext.xml", "spring-security.xml" })
+@ImportResource({"prod/applicationContext.xml","spring-security.xml"})
 
 public class Application {
 
 	/**
 	 * @param args
 	 */
-
+	
 	public static void main(String[] args) {
 		LoggerContext context = (org.apache.logging.log4j.core.LoggerContext) LogManager.getContext(false);
-		if (args.length != 1) {
-			System.err.println("need a location for log file");
-			return;
-		}
-
-		File file = new File(args[0]);
-		if (file.exists()) {
-			if (file.isDirectory()) {
-				System.err.println("it should be a file, not a directory for log file");
-			}
-		}
-
+		File file = new File("log4j2.xml");
+		 
 		// this will force a reconfiguration
 		context.setConfigLocation(file.toURI());
-
+		
+	
 		SpringApplication.run(Application.class, args);
 
 	}
